dcd9d1a10141c5c8cde5978ad4423bf448f50c1b
Rasel2276 <mdraselhossain2276@gmail.com>
2025-10-17 13:47:53 +0600

    up

--- multi-vendor-ecommerce/routes/web.php
+++ multi-vendor-ecommerce/routes/web.php
@@ -15,8 +15,12 @@ use App\Http\Controllers\Customer\CustomerMainController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('front_end.home.website');
 });
 
 // admin routes //
