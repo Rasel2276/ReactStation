37a30041961910de440b617b78aef2ffd2421f6e
Rasel2276 <mdraselhossain2276@gmail.com>
2025-10-27 07:52:20 +0600

    up

---
+++ multi-auth/app/Models/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * Mass assignable attributes
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',    // admin, vendor, customer
+        'status',  // pending, approved, rejected
+    ];
+
+    /**
+     * Hidden attributes for serialization
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Attribute casting
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
