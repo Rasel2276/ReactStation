7f01c6c68e93e84a24e0eb46dbc0d99935b27de2
Rasel2276 <mdraselhossain2276@gmail.com>
2025-11-26 00:05:14 +0600

    up

--- React-Laravel-Multi-Vendor-Ecommerce/back-laravel/app/Http/Controllers/AuthController.php
+++ React-Laravel-Multi-Vendor-Ecommerce/back-laravel/app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
         $user = User::create([
             'name'=>$request->name,
             'email'=>$request->email,
-            'password'=>Hash::make($request->password), // hashed password
+            'password'=>Hash::make($request->password), // hashed password //
             'role'=>$request->role
         ]);
 
